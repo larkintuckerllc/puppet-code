5511257f2a5c395d6ccd64527c1908abfecdc2e9
John Tucker <john@larkintuckerllc.com>
2020-11-15 11:15:34 -0500

    stub

---
+++ environments/production/modules/my_parameters/manifests/my_class.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include my_parameters::my_class
+class my_parameters::my_class {
+}
