2082c060b196a9811e1d215ebebb561c45fb99d9
John Tucker <john@larkintuckerllc.com>
2020-11-16 09:50:39 -0500

    stub

---
+++ environments/production/modules/my_parameters_refactor/manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include my_parameters_refactor
+class my_parameters_refactor {
+}
