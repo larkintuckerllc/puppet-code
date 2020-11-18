59b6b4fb1b8868ad95d08355c926100828be5a08
John Tucker <john@larkintuckerllc.com>
2020-11-18 17:12:05 -0500

    scaffold module

---
+++ environments/production/modules/my_files_templates/manifests/init.pp
@@ -0,0 +1,10 @@
+# @summary Files and templates
+#
+# Files and templates
+#
+# @example
+#   include my_files_templates
+class my_files_templates {
+  include my_files_templates::my_files
+  include my_files_templates::my_templates
+}
---
+++ environments/production/modules/my_files_templates/manifests/my_files.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include my_files_templates::my_files
+class my_files_templates::my_files {
+}
