00f3f5000aedec6de47809356ee20e3d16549575
John Tucker <john@larkintuckerllc.com>
2020-11-18 20:59:02 -0500

    fixed

--- environments/production/modules/my_files_templates/manifests/my_templates.pp
+++ environments/production/modules/my_files_templates/manifests/my_templates.pp
@@ -6,8 +6,8 @@
 #   include my_files_templates::my_templates
 class my_files_templates::my_templates {
   file { '/tmp/hello_template':
-    ensure => 'present',
-    path   => '/tmp/hello_template',
-    source => epp('my_files_templates/hello_template.epp')
+    ensure  => 'present',
+    content => epp('my_files_templates/hello_template.epp'),
+    path    => '/tmp/hello_template',
   }
 }
